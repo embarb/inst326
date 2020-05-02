44a9fd10f6ddd315b55a1b7bb6e67c66a2f94686
gliitz <63757537+gliitz@users.noreply.github.com>
2020-05-01 22:19:23 -0400

    Update group.py

--- group.py
+++ group.py
@@ -13,4 +13,34 @@ class Student:
         self.birthdate = birth_date
         self.gpa = gpa
         self.major = major
+        self.grade = []
+        self.credits = credits
+
+    def read_grades(self, file_path):
+        temp_grades = []
+        temp_credits = []
+        file = open(file_path, "r", encoding='utf-8')
+        with file:
+            line = (file.readline()).split()
+            if (len(line) !=0):
+                temp_grades.append(line[0])
+                temp_credits.append(float(line[1]))
+           
+    def calculate_gpa(self):
+        totalValue = 0
+        totalNumberOfCredits = 4 + 3 + 2 + 1 + 0
+        for (grade, credits) in self.grade:  
+           if (grade[0] == 'A'):
+               totalValue += (4.0*credits[1])                   
+           elif(grade[0] == 'B'):
+               totalValue += (3.0*credits[1])
+           elif(grade[0] == 'C'):
+               totalValue += (2.0*credits[1])
+           elif(grade[0] == 'D'):
+               totalValue += (1.0*credits[1])
+           elif(grade[0] == 'F'):
+               totalValue += (0.0*credits[1])
+        total_gpa = totalValue/totalNumberOfCredits
+        return total_gpa 
+
 
