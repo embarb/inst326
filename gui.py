5918d745418c818c5360151cd97a95254afaa568
embarb <63757178+embarb@users.noreply.github.com>
2020-05-01 23:18:08 -0400

    Updated Buttons
    Added register_student and student_schedule so that when you click those buttons, a new window appears where the webscraping information will go. Needs to be cleaned up more and window resized.

--- gui.py
+++ gui.py
@@ -12,21 +12,23 @@ class GUI(tk.Frame):
         self.register["text"] = "Register Student"
         self.register["command"] = self.register_student
         self.register.pack(side="left")
-        #self.schedule = tk.Button(self)
-        #self.schedule["text"] = "Student Schedule"
-        #self.schedule["command"] = self.student_schedule
-        #self.schedule.pack(side="right")
+        self.schedule = tk.Button(self)
+        self.schedule["text"] = "Student Schedule"
+        self.schedule["command"] = self.student_schedule
+        self.schedule.pack(side="right")
 
         self.quit = tk.Button(self, text="QUIT", fg="red",
                               command=self.master.destroy)
         self.quit.pack(side="bottom")
 
     def register_student(self):
-        self.register_window = tk.Toplevel(root)
-        self.label_register_window = tk.Label(register_window, text = "Student Registration")
-        self.label_register_window.pack()
-        self.label_register_button = tk.Button(app, text="Register Button", command=register_window)
-        self.label_register_button.pack()
+        self.register_window = tk.Toplevel(self)
+        self.register_window_button = tk.Button(self, text="Submit") #going to equal submitting their new schedule
+        
+    def student_schedule(self):
+        self.schedule_window = tk.Toplevel(self)
+        self.schedule_window_button = tk.Button(self, text="Submit") #going to equal submitting their new schedule
+        
 
 root = tk.Tk()
 app = GUI(master=root)
