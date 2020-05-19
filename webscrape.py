ba7a8ba84523e722eee70579b3f90d59f8ed54ee
vtann17 <63757494+vtann17@users.noreply.github.com>
2020-05-19 17:17:50 -0400

    Update webscrape.py

--- webscrape.py
+++ webscrape.py
@@ -1,22 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
 import time
+import coursescrape
 
-#web scrape testudo schedule of classes. first get links for all majors and then scrape all course information.
-
+#web scrape testudo schedule of classes. first create list of majors of courses. then create urls for each course. finally, extract
+#info from each section of courses.
 time.sleep(1)
 page = requests.get("https://app.testudo.umd.edu/soc/")
 soup = BeautifulSoup(page.text, "html.parser")
-links = soup.findAll("a")
+major_links = soup.findAll("a")
 
-i = 26
-while i < len(links) - 5:
-    url = "https://app.testudo.umd.edu/soc/" + links[i]["href"]
-    print(url)
-    
-    #test
-    #new_page = requests.get(url)
-    #time.sleep(1)
-    #new_soup = BeautifulSoup(page.text, "html.parser")
-    #info = soup.findAll("div") 
+url_list = []
+course_list = []
+
+i = 11
+while i < len(major_links) - 5:
+    url = "https://app.testudo.umd.edu/soc/" + major_links[i]["href"]
+    url_list.append(url)
     i += 1
+
+
+for url in url_list:
+    course_url = coursescrape.course_scrape(url)
+    course_list.append(course_url)
+    
+for course in course_list:
+    coursescrape.section_scrape(course)
