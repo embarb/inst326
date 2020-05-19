f720a3d4270feda5bb8b8b9843e787daaa21bf17
elvisaguilar704 <63757562+elvisaguilar704@users.noreply.github.com>
2020-05-19 13:05:19 -0400

    Update schedule.py

--- schedule.py
+++ schedule.py
@@ -1,16 +1,23 @@
+#the lists below are to test it
+course = ["stat 100", "inst 326", "psyc 100", "math 115", "uslt 202"]
+time_M = ["9-9:50", "10-10:30", "11-11:30", "12-12:30", "1-1:30"]
+time_T = ["11-12:15", "10-10:40", "1-1:40", "12-12:40", "1-1:40"]
+time_W = ["9-9:50", "10-10:30", "11-11:30", "12-12:30", "1-1:30"]
+time_TH = ["11-12:15", "10-10:40", "11-11:40", "12-12:40", "1-1:40"]
+time_F = ["9-9:50", "10-10:30", "11-11:30", "12-12:30", "1-1:30"]
+professor =["Griffin", "Gabriel", "Selterman", "Rosca", "Chester"]
+building =["ESJ 2208", "BPS 1236", "BPS 1101", "ESJ 0202", "TWS 0328"]
+
+
 class Student_Schedule:
     def __init__(self): 
-        #Course abbreviation
-        self.course = course 
-        #Times Monday-Friday
-        self.time_M = time_M 
-        self.time_T = time_T 
+        self.course = course
+        self.time_M = time_M
+        self.time_T = time_T
         self.time_W = time_W
         self.time_TH = time_TH
         self.time_F = time_F
-        #Professor's name
         self.professor = professor
-        #Building and room #
         self.building = building
         
         
@@ -18,8 +25,10 @@ class Student_Schedule:
 class Schedule(Student_Schedule):
     def __init__(self):
         Student_Schedule.__init__(self)
+        
     
-#Nested list of each class and its information (course, times, professor, building and room #)
+    
+
     def schedule(self):
         nested_list = [
             [self.course[0], self.time_M[0], self.time_T[0], self.time_W[0], self.time_TH[0], self.time_F[0], self.professor[0], self.building[0]], 
@@ -33,10 +42,10 @@ class Schedule(Student_Schedule):
             [self.course[4], self.time_M[4], self.time_T[4], self.time_W[4], self.time_TH[4], self.time_F[4], self.professor[4], self.building[4]],
         ]
 
-        print (": Course : Time(Monday) : Time(Tuesday) : Time(Wednesday) : Time(Thursday) : Time(Friday : Professor's Name : Building and Room # :")
+        print (": Course Name : Time(Monday) : Time(Tuesday) : Time(Wednesday) : Time(Thursday) : Time(Friday : Professor's Name : Building and Room # :")
 
         for item in nested_list:
-            print(":", item[0], " "*(5-len(item[0])), ":",
+            print(":", item[0], " "*(10-len(item[0])), ":",
                 item[1], " "*(11-len(item[1])), ":",
                 item[2], " "*(12-len(item[2])), ":",
                 item[3], " "*(14-len(item[3])), ":",
@@ -45,7 +54,7 @@ class Schedule(Student_Schedule):
                 item[6], " "*(15-len(item[6])), ":",
                 item[7], " "*(18-len(item[7])), ":", )
             
-#prints the class information under each category
+
 if __name__ == "__main__":
     sch = Schedule()
     sch.schedule()
