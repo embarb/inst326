2a80f8badf63c31c266f8aa3126e9ef3d7a60e60
embarb <63757178+embarb@users.noreply.github.com>
2020-05-19 22:53:08 -0400

    Updated Student.py

--- student.py
+++ student.py
@@ -1,50 +1,22 @@
-def the_name(fname, lname):
-    return tuple([fname.upper(), lname.upper()])
-
-def birth_date(birthdate):
-    day, month, year = birthdate.split(',')
-    birth_date = {'day':day, 'month':month, 'year':year}
-    return birth_date
-
 class Student:
-    def __init__(self, id, fname, lname, birthdate, gpa, major, grade, credits):
+    def __init__(self, id, fname, lname, day, month, year, gpa, major):
         self.id = id
-        self.name = the_name
-        self.birthdate = birth_date
+        self.fname = fname
+        self.lname = lname
+        self.day = day
+        self.month = month
+        self.year = year
         self.gpa = gpa
         self.major = major
-        self.grade = []
-        self.credits = credits
 
-    def read_grades(self, file_path):
-        temp_grades = []
-        temp_credits = []
-        file = open(file_path, "r", encoding='utf-8')
-        with file:
-            line = (file.readline()).split()
-            if (len(line) !=0):
-                temp_grades.append(line[0])
-                temp_credits.append(float(line[1]))
-           
-    def calculate_gpa(self):
-        overall_value = 0
-        all_credits = 4 + 3 + 2 + 1 + 0
-        for (grade, credits) in self.grade:  
-           if (grade[0] == 'A'):
-               totalValue += (4.0*credits[1])                   
-           elif(grade[0] == 'B'):
-               totalValue += (3.0*credits[1])
-           elif(grade[0] == 'C'):
-               totalValue += (2.0*credits[1])
-           elif(grade[0] == 'D'):
-               totalValue += (1.0*credits[1])
-           elif(grade[0] == 'F'):
-               totalValue += (0.0*credits[1])
-        sum_gpa = overall_value/all_fCredits
-        return sum_gpa 
+    def print_info(self, id, fname, lname, day, month, year, gpa, major):
+        print("Student ID: " + self.id)
+        print("Student Name: " + self.fname + " " + self.lname)
+        print("Student Birthdate: " + self.month + "/" + self.day + "/" + self.year)
+        print("Student GPA: " + self.gpa)
+        print("Student Major " + self.major)
 
-student = Student('ID ="12356746" ', 'fname ="Emi" ', 'lname ="Bieber" ', 'birthdate ="26,October,1998" ', 'gpa="3.8" ', 'major="InfoSci" ',
-'grade="[]" ', 'credits="70" ')
-student.calculate_gpa()
+if __name__ == "__main__":
+    final = Student('92038083', 'emily', 'barbagallo', '10', '3', '1999', '4.0', 'Information Science')
+    print(final.print_info('92038083', 'emily', 'barbagallo', '10', '3', '1999', '4.0', 'Information Science'))
 
-print(student)
